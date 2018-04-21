255a122b1d7d723903a7e4f7ac197d14af342aa2
Michiel Sikma <michiel@sikma.org>
2018-04-21 19:28:36 +0200

    New build - 1.2.1

--- dist/mandarake/index.js
+++ dist/mandarake/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.mandarakeSearch = undefined;
+exports.getMandarakeFavorites = exports.mandarakeSearch = undefined;
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; /**
                                                                                                                                                                                                                                                                    * mdrscr - Mandarake Scraper <https://github.com/msikma/mdrscr>
@@ -17,9 +17,11 @@ var _categories = require('./categories');
 var _shops = require('./shops');
 
 // Mandarake mail order site.
-var orderURL = _scrape.MANDARAKE_ORDER_BASE + '/order/listPage/list?';
+var MANDARAKE_ORDER_URL = _scrape.MANDARAKE_ORDER_BASE + '/order/listPage/list';
 // Mandarake auction site.
-var auctionURL = _scrape.MANDARAKE_AUCTION_BASE + '/auction/item/itemsListEn.html?category=plamo';
+var MANDARAKE_AUCTION_URL = _scrape.MANDARAKE_AUCTION_BASE + '/auction/item/itemsListEn.html?category=plamo';
+// Page URL for a Mandarake user's favorites list.
+var MANDARAKE_FAVS_URL = _scrape.MANDARAKE_ORDER_BASE + '/order/MyPage/favoritesList';
 
 // Default search details. Every search overrides these values.
 // You can change anything, but it's not recommended to change 'sort', 'sortOrder' and 'dispCount'.
@@ -69,8 +71,16 @@ var defaultDetails = {
 var mandarakeSearchURL = function mandarakeSearchURL(searchDetails, lang) {
   return (
     // Converts our search parameters to a query string.
-    '' + orderURL + objToParams(_extends({}, searchDetails, { lang: lang }))
+    MANDARAKE_ORDER_URL + '?' + objToParams(_extends({}, searchDetails, { lang: lang }))
   );
+};
+
+/**
+ * Returns the URL we need to scrape the user's favorites.
+ * We just add the language, that's all.
+ */
+var mandarakeFavoritesURL = function mandarakeFavoritesURL(lang) {
+  return MANDARAKE_FAVS_URL + '?' + objToParams({ lang: lang });
 };
 
 /**
@@ -83,4 +93,21 @@ var mandarakeSearch = exports.mandarakeSearch = function mandarakeSearch(searchD
   var search = _extends({}, defaultDetails, searchDetails);
   var url = mandarakeSearchURL(search, lang);
   return (0, _scrape.fetchMandarakeSearch)(url, search, lang);
+};
+
+/**
+ * Runs a search for a user's Mandarake favorites and returns either just the basic info
+ * (same as the search result info) or also extended shop availability info.
+ *
+ * A progress callback can be passed. This will be called during the lengthy detail
+ * fetching phase, which can take a long time if there are a lot of favorites.
+ * If we need to get extended info, we need to request the detail pages for all those items.
+ * The callback takes the signature (currItem, totalItems), both numbers.
+ */
+var getMandarakeFavorites = exports.getMandarakeFavorites = function getMandarakeFavorites() {
+  var lang = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'ja';
+  var getExtendedInfo = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+  var progressCb = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+
+  return (0, _scrape.fetchMandarakeFavorites)(mandarakeFavoritesURL(lang), lang, getExtendedInfo, progressCb);
 };
